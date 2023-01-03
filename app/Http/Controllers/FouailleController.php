83a85cc3657e4fa6c7974f57483a680976da1aab
root <info.telecom.strasbourg@gmail.com>
2023-01-03 15:50:31 +0100

    lisibilité

--- app/Http/Controllers/FouailleController.php
+++ app/Http/Controllers/FouailleController.php
@@ -9,19 +9,26 @@ use App\Models\Fouaille;
 class FouailleController extends Controller
 {
     public function fouaille(Request $request){
+        // Récupère l'utilisateur connecté
         $user = DB::table('users')->select('nom')->where('identifiant', '=', $request->session()->get('cas_user'))->first();
         $prenom = explode(" ", $user->nom)[0];
         $nom = explode(" ", $user->nom)[1];
 
+        // Va chercher dans la BDD du BDE 
         $fouaille[] = new Fouaille;
         $fouaille = DB::connection('bde')
-        ->table('Commande')
-        ->select('new_note','delta','type_produit','date_histo')
-        ->where('nom', '=', $nom)
-        ->where('prenom', '=', $prenom)
-        ->orderBy('date_histo', 'desc')
-        ->limit(20)
-        ->get();
+            ->table('Commande')
+            ->select(
+                    'date_histo',
+                    'new_note',
+                    'delta',
+                    'type_produit',
+                    )
+            ->where('nom', '=', $nom)
+            ->where('prenom', '=', $prenom)
+            ->orderBy('date_histo', 'desc')
+            ->limit(20)
+            ->get();
         return $fouaille;
     }
 }
