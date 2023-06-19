a160c6b10c6395b35963586843e19a2b9e299c02
ITS-TPS <info.telecom.strasbourg@gmail.com>
2023-06-19 14:13:52 +0200

    fix to get also order with null product

--- app/Http/Controllers/FouailleController.php
+++ app/Http/Controllers/FouailleController.php
@@ -23,18 +23,18 @@ class FouailleController extends Controller
             'products.title',
             'products.color',
             )
-        ->join('products', 'orders.product_id', '=', 'products.id')
+        ->leftJoin('products', 'orders.product_id', '=', 'products.id')
         ->where('orders.member_id', '=', $user->bde_id)
         ->get();
 
         $balance = DB::connection('bde_bdd')->table('members')->where('id', '=', $user->bde_id)->first()->balance;
         
-        $data = $fouaille->map(function ($item) {
+        $orders = $fouaille->map(function ($item) {
             return [
                 'date' => $item->date,
                 'total_price' => $item->price,
                 'amount' => $item->amount,
-                'product' => [
+                'product' => ($item->name == null) ? null : [
                     'name' => $item->name,
                     'title' => $item->title,
                     'unit_price' => strval($item->price / $item->amount),
@@ -49,7 +49,7 @@ class FouailleController extends Controller
                 'first_name' => $user->first_name,
                 'last_name' => $user->last_name,
                 'user_name' => $user->user_name,
-                'orders' => $data
+                'orders' => $orders
             ],
         ], 200);
     }
