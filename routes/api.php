69bf8a66789dbd1118373f840fc02d21d1a59f1c
Thibaut DESLANDES <deslandes.thibaut@gmail.com>
2024-09-01 13:23:17 +0200

    user_is_author pour les commentaires et user avatar

--- routes/api.php
+++ routes/api.php
@@ -126,7 +126,7 @@ Route::get('crous', [CrousController::class, 'index'])
             Route::post('/avatar', [UserAvatarController::class, 'store'])
             ->name('user.avatar.store');
 
-            Route::post('avatar/default', [UserAvatarController::class, 'storedefault'])
+            Route::post('/avatar/default', [UserAvatarController::class, 'storedefault'])
                 ->name('user.avatar.default');
 
             Route::delete('/', [UserController::class, 'delete'])
